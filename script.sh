cf6166a1425593cf3086a8897b39a470aad5c048
HUYNH HA MAI TRINH <hhmtrinh@apcs.vn>
2019-09-04 14:25:53 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -26,7 +26,7 @@ echo "service started"
 #download ngrok 
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
-./ngrok authtoken 2oA4spcYkfLFkwtG8rsfW_7EANBPmCAFC6KAoSZ2jeU
+./ngrok authtoken 1QLwws2PclwortZn4TQ46kFdjPm_3ttdFF2cyqd6wJkxyNC94
 
 echo "ngrok stated"
 
